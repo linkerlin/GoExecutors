2b7261d2c1d124545c78d4da91b5c6aab5d5fc34
Linker Lin <linkerlin@icloud.com>
2016-12-31 16:57:01 +0800

    遇到一个奇怪的错误

--- src/executors/executors.go
+++ src/executors/executors.go
@@ -1,4 +1,19 @@
 package executors
 
 type Executors struct {
+	callableQ chan func() interface{}
+}
+
+func NewExecutors() *Executors {
+	var cq = make(chan func() interface{}, 100)
+	go func() {
+		var callable func() interface{}
+		callable <- cq
+	}()
+	return &Executors{cq}
+
+}
+
+func (es *Executors) Submit(callable func() interface{}) {
+	es.callableQ <- callable
 }
--- src/goexecutors/main.go
+++ src/goexecutors/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"config"
+	"executors"
 	"fmt"
 )
 
 func main() {
 	config.LoadConfig()
 	fmt.Println("Default goroutines number is ", config.DefaultGoroutinesNum())
-
+	es := executors.NewExecutors()
+	es.Submit(func() interface{} {
+		fmt.Println("这是从一个Callable内部发出的声音。")
+		return 1
+	})
 }
