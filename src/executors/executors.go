a25ac594035a1391691fae096ddd1787debda3e7
Linker Lin <linkerlin@icloud.com>
2016-12-31 19:56:00 +0800

    有一个问题，future从rets里面获取不到chan

--- src/executors/executors.go
+++ src/executors/executors.go
@@ -2,7 +2,9 @@ package executors
 
 import (
 	"config"
+	"fmt"
 	"sync"
+	"time"
 )
 
 type Callable func() interface{}
@@ -11,21 +13,50 @@ type CallableQ chan Callable
 type Executors struct {
 	callableQ CallableQ
 	lock      *sync.Mutex
-	rets      map[*Callable]interface{}
+	rets      map[*Callable]chan interface{}
+}
+
+type Future struct {
+	es       *Executors
+	callable *Callable
+}
+
+func NewFuture(es *Executors, callable *Callable) *Future {
+	return &Future{es, callable}
+}
+
+func (f *Future) GetResult(timeout time.Duration) interface{} {
+	fmt.Println("future get result.", f, timeout)
+	timer := time.NewTimer(timeout)
+	var ret interface{}
+	defer delete(f.es.rets, f.callable)
+	fmt.Println("future对应的结果chan:", f.es.rets[f.callable])
+	select {
+	case ret = <-f.es.rets[f.callable]:
+		fmt.Println("future 获取到了结果：", ret)
+		return ret
+	case <-timer.C:
+		return nil
+	}
+
 }
 
 func NewExecutors() *Executors {
 	var cq = make(CallableQ, 100)
-	var es = &Executors{cq, &sync.Mutex{}, make(map[*Callable]interface{})}
+	var es = &Executors{cq, &sync.Mutex{}, make(map[*Callable]chan interface{})}
 	for i := 0; i < config.DefaultGoroutinesNum(); i++ {
 		go func() {
 			var callable Callable
 			for {
 				callable = <-cq
+				es.rets[&callable] = make(chan interface{}, 1) // 保证map里面有东西
 				var ret = callable()
+				fmt.Println("callable ret:", ret, " to ", es.rets[&callable])
 				es.lock.Lock()
 				defer es.lock.Unlock()
-				es.rets[&callable] = ret
+				var retChan = es.rets[&callable]
+				fmt.Println("result chan:", retChan)
+				retChan <- ret
 			}
 		}()
 	}
@@ -34,6 +65,7 @@ func NewExecutors() *Executors {
 
 }
 
-func (es *Executors) Submit(callable func() interface{}) {
+func (es *Executors) Submit(callable Callable) *Future {
 	es.callableQ <- callable
+	return NewFuture(es, &callable)
 }
--- src/goexecutors/main.go
+++ src/goexecutors/main.go
@@ -11,9 +11,17 @@ func main() {
 	config.LoadConfig()
 	fmt.Println("Default goroutines number is ", config.DefaultGoroutinesNum())
 	es := executors.NewExecutors()
-	es.Submit(func() interface{} {
+	var future = es.Submit(func() interface{} {
 		fmt.Println("这是从一个Callable内部发出的声音。")
+		//		time.Sleep(time.Second * 1)
 		return 1
 	})
-	time.Sleep(10000)
+	var ret = future.GetResult(time.Millisecond * 1500)
+	switch ret {
+	case nil:
+		fmt.Println("超时！")
+	default:
+		fmt.Println("执行成功", ret)
+	}
+	time.Sleep(time.Second * 3)
 }
