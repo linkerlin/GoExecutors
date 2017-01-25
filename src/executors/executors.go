b8782a78390bef39e113f1ceff5e3bb737f54990
Linker Lin <linkerlin@icloud.com>
2017-01-25 22:17:14 +0800

    callable can return result and error

--- src/executors/executors.go
+++ src/executors/executors.go
@@ -3,66 +3,70 @@ package executors
 import (
 	"config"
 	"fmt"
+	"sync/atomic"
 	"time"
 )
-
-type ErrorFuture interface{}
 
 type ErrorTimeout string
 
 func (e ErrorTimeout) Error() string { return string(e) }
 
-type Callable func() interface{}
-type CallableQ chan func() (chan interface{}, chan interface{}, Callable)
+type Callable func() (interface{}, error) // result + error
+type FutureFuncQ chan func() (future *Future)
 
 type Executors struct {
-	callableQ CallableQ
+	futureFuncQ FutureFuncQ
+	goNum       int32
 }
 
 type Future struct {
-	retChan   chan interface{}
-	errorChan chan interface{}
+	retChan       chan interface{}
+	errorChan     chan error
+	exceptionChan chan interface{}
+	callable      Callable
 }
 
-func NewFuture(retChan, errorChan chan interface{}) *Future {
-	return &Future{retChan, errorChan}
-}
-
-func (f *Future) GetResult(timeout time.Duration) (ret interface{}, timeoutError error, err ErrorFuture) {
+func (f *Future) GetResult(timeout time.Duration) (ret interface{}, timeoutError error, err error, exception interface{}) {
 	timer := time.NewTimer(timeout)
 	// fmt.Println("future对应的结果chan:", f.retChan)
 	select {
-	case ret = <-f.retChan:
-		fmt.Println("future 获取到了结果：", ret)
-		return ret, nil, nil
+	case exception = <-f.exceptionChan:
+		fmt.Println("future 获取到了异常：", err)
+		return nil, nil, nil, exception
 	case err = <-f.errorChan:
 		fmt.Println("future 获取到了错误：", err)
-		return nil, nil, err
+		return nil, nil, err, nil
+	case ret = <-f.retChan:
+		fmt.Println("future 获取到了结果：", ret)
+		return ret, nil, nil, nil
 	case <-timer.C:
-		return nil, ErrorTimeout("Callable执行超时错误！"), nil
+		return nil, ErrorTimeout("Callable执行超时错误！"), nil, nil
 	}
-
 }
 
 func NewExecutors() *Executors {
-	var cq = make(CallableQ, 100)
-	var es = &Executors{cq}
+	var cq = make(FutureFuncQ, 100)
+	var es = &Executors{cq, 0}
 	for i := 0; i < config.DefaultGoroutinesNum(); i++ {
 		go func() {
+			atomic.AddInt32(&es.goNum, 1)
+			defer atomic.AddInt32(&es.goNum, -1)
 			for {
 				var err interface{}
-				cf := <-cq
-				retChan, errorChan, callable := cf()
+				futureFunc := <-cq
+				future := futureFunc()
 				defer func() {
 					if err = recover(); err != nil {
 						fmt.Errorf("捕获了一个错误:%v", err)
-						errorChan <- err
+						future.exceptionChan <- err
 					}
 				}()
-				ret := callable()
+				ret, callableError := future.callable()
 				// fmt.Println("result chan:", retChan)
-				if retChan != nil {
-					retChan <- ret
+				if callableError != nil {
+					future.errorChan <- callableError
+				} else if ret != nil {
+					future.retChan <- ret
 				}
 				fmt.Println(".")
 			}
@@ -75,10 +79,12 @@ func NewExecutors() *Executors {
 
 func (es *Executors) Submit(callable Callable) *Future {
 	retChan := make(chan interface{}, 1)
-	errorChan := make(chan interface{}, 1)
-	c := func() (chan interface{}, chan interface{}, Callable) {
-		return retChan, errorChan, callable
+	errorChan := make(chan error, 1)
+	exceptionChan := make(chan interface{}, 1)
+	futureFunc := func() *Future {
+		return &Future{retChan, errorChan, exceptionChan, callable}
 	}
-	es.callableQ <- c
-	return NewFuture(retChan, errorChan)
+
+	es.futureFuncQ <- futureFunc
+	return futureFunc()
 }
--- src/goexecutors/main.go
+++ src/goexecutors/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"config"
+	"errors"
 	"executors"
 	"fmt"
 	"time"
@@ -11,17 +12,19 @@ func main() {
 	config.LoadConfig()
 	fmt.Println("Default goroutines number is ", config.DefaultGoroutinesNum())
 	es := executors.NewExecutors()
-	f := func() interface{} {
+	f := func() (interface{}, error) {
 		fmt.Println("这是从一个Callable内部发出的声音。")
 		//		time.Sleep(time.Second * 1)
-		return 1
+		return 1, nil
 	}
 
 	var future = es.Submit(f)
-	var ret, t, e = future.GetResult(time.Millisecond * 1500)
+	var ret, t, e, ex = future.GetResult(time.Millisecond * 1500)
 	switch {
+	case ex != nil:
+		fmt.Println("异常", ex)
 	case t == nil && e == nil:
-		fmt.Println("正常", ret)
+		fmt.Println("No.1 正常", ret)
 	case t != nil:
 		fmt.Println("超时！")
 	case e != nil:
@@ -29,32 +32,36 @@ func main() {
 	default:
 		fmt.Println("不会到这里", ret)
 	}
-	fTimeout := func() interface{} {
+	fTimeout := func() (interface{}, error) {
 		time.Sleep(time.Second * 1)
 		fmt.Println("这是第二次从Callable内部发出的声音。")
-		return 2
+		return 2, errors.New("1s")
 	}
 	fmt.Println("=================")
 	time.Sleep(100)
 	future = es.Submit(fTimeout)
-	ret2, t, err := future.GetResult(time.Millisecond * 500)
+	ret2, t, err, ex := future.GetResult(time.Millisecond * 500)
 	switch {
+	case ex != nil:
+		fmt.Println("异常", ex)
 	case t == nil && err == nil:
 		fmt.Println("执行成功", ret2)
 	case err != nil:
 		fmt.Println("执行出错", err)
 	case t != nil:
-		fmt.Println("超时！", t)
+		fmt.Println("No.2 超时！", t)
 	default:
 		fmt.Println("不会到这里", ret2)
 	}
-	fPanic := func() interface{} {
+	fPanic := func() (interface{}, error) {
 		fmt.Println("这是第三次从Callable内部发出的声音。")
 		panic(100)
 	}
 	future = es.Submit(fPanic)
-	ret3, t, err := future.GetResult(time.Millisecond * 500)
+	ret3, t, err, ex := future.GetResult(time.Millisecond * 500)
 	switch {
+	case ex != nil:
+		fmt.Println("No.3 异常", ex)
 	case err == nil && t == nil:
 		fmt.Println("执行失败,没有捕获到错误", ret3)
 	case t != nil:
@@ -64,5 +71,27 @@ func main() {
 	default:
 		fmt.Println("不会到这里", ret3)
 	}
+
+	f = func() (interface{}, error) {
+		fmt.Println("这是从No.4 Callable内部发出的声音。")
+		//		time.Sleep(time.Second * 1)
+		return 1, errors.New("😀")
+	}
+
+	future = es.Submit(f)
+	ret, t, e, ex = future.GetResult(time.Millisecond * 1500)
+	switch {
+	case ex != nil:
+		fmt.Println("异常", ex)
+	case t == nil && e == nil:
+		fmt.Println("正常", ret)
+	case t != nil:
+		fmt.Println("超时！")
+	case e != nil:
+		fmt.Println("No.4 出错", e)
+	default:
+		fmt.Println("不会到这里", ret)
+	}
+
 	time.Sleep(time.Second * 6)
 }
